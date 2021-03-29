629d282b5bce2997f53ab151587d2a8888f60891
Maciej Para <m.para@nogravity.io>
2021-03-29 21:15:21 +0200

    cat model example

---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+import { IsInt, IsString } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  readonly name: string;
+
+  @IsInt()
+  readonly age: number;
+
+  @IsString()
+  readonly breed: string;
+}
