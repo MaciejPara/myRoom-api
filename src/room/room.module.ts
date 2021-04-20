1ec4168efd68dae498a632b1811efab211bc3242
Maciej Para <m.para@nogravity.io>
2021-04-20 20:37:59 +0200

    initialize room database with testing object

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -3,6 +3,8 @@ import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
 import { DatabaseModule } from '../database/database.module';
 import { roomProviders } from './room.providers';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { Room } from './interfaces/room.interface';
 
 @Module({
   imports: [DatabaseModule],
@@ -10,4 +12,25 @@ import { roomProviders } from './room.providers';
   controllers: [RoomController],
   exports: [RoomService],
 })
-export class RoomModule {}
+export class RoomModule {
+  private room: CreateRoomDto = {
+    title: 'Room',
+    shortDescription: 'shortDescription',
+    description: 'description',
+    imgUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/13/db/45/9e/dolmen-hotel-malta.jpg',
+  };
+
+  constructor(private readonly roomService: RoomService) {
+    this.initTestingRoom();
+  }
+
+  async initTestingRoom() {
+    const allRooms: Room[] = await this.roomService.findAll();
+    const isExistingRoom: boolean = allRooms.length > 0;
+
+    if (!isExistingRoom) {
+      await this.roomService.create(this.room);
+    }
+  }
+}
